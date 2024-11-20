6efd1dc77ebdae6f1a61e4b28cea2541d44bcc5d
Dragonslayer9463 <djeacott@gilbertschools.net>
2024-11-20 07:47:49 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let penguin = sprites.create(img`
+    . . . f f f f f f f f f f . . . 
+    . . f 6 6 6 6 6 6 6 6 6 6 f . . 
+    . . f 6 6 6 6 6 6 6 6 6 6 f . . 
+    . f 6 6 1 f 6 6 6 6 1 f 6 6 f . 
+    . f 6 6 f f 6 6 6 6 f f 6 6 f . 
+    . f 6 6 6 4 4 4 4 4 4 6 6 6 f . 
+    . f 6 6 6 6 4 4 4 4 6 6 6 6 f . 
+    f 6 6 6 6 1 1 1 1 1 1 6 6 6 6 f 
+    f 6 6 6 1 1 1 1 1 1 1 1 6 6 6 f 
+    f 6 f 1 1 1 1 1 1 1 1 1 1 f 6 f 
+    . f f 1 1 1 1 1 1 1 1 1 1 f f . 
+    . . f 1 1 1 1 1 1 1 1 1 1 f . . 
+    . . f 4 4 4 f f f f 4 4 4 f . . 
+    . . . f f f . . . . f f f . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+penguin.sayText(":)")
